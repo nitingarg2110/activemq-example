322f7a2ba1997b8641ec70fafb9375b082830955
ab73480 <ab73480@X7016567.ctl.intranet>
2018-03-04 16:35:11 -0700

    kkk

--- src/main/java/com/nitin/activemq/activemqeaxample/listener/Consumer.java
+++ src/main/java/com/nitin/activemq/activemqeaxample/listener/Consumer.java
@@ -13,17 +13,12 @@ public class Consumer {
 	@Autowired
 	private DetermineMicroservice determineService;
 
-	public Consumer(DetermineMicroservice determineService) {
-		super();
-		this.determineService = determineService;
-	}
-
 	@JmsListener(destination = "standalone.queue", containerFactory = "jsaFactory")
 	public void consumer(SeoEvent seoEvent) {
 		System.out.println("Recieved Message " + seoEvent);
-		
+
 		determineService.getMicroServiceName(seoEvent).getMicroServiceName();
-		
+
 		System.out.println("Service Name:" + determineService.getMicroServiceName(seoEvent).getMicroServiceName());
 
 	}
