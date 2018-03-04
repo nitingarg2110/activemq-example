71e7cdf7b36b624de374a72044ded0ed4dcdd3ba
Garg <nitin.garg@centurylink.com>
2018-03-03 19:14:50 -0700

    first commit

---
+++ src/main/java/com/nitin/activemq/activemqeaxample/ActivemqEaxampleApplication.java
@@ -0,0 +1,12 @@
+package com.nitin.activemq.activemqeaxample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqEaxampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqEaxampleApplication.class, args);
+	}
+}
